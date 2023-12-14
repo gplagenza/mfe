31e5d3afcd0a6a0b08f81b8bc25bdb90df7fa14f
Plagenza <gplagenza@deloitte.it>
2023-12-14 18:08:33 +0100

    Implemented navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'; 
 import App from './App';
   
-const mount = (el) => {
-    ReactDOM.render(<App />, el)
+const mount = (el, { onNavigate, defaultHistory } ) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate);
+    };
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname}) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 };
 
 // mount function to start up the app
@@ -14,7 +28,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
